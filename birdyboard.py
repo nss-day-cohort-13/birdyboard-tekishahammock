c9b87f926d31061ffd89b28fde93bc4b5c422cbc
Tekisha Hammock <tekisha.hammock@gmail.com>
2016-08-01 15:57:45 -0500

    initial file set up and commented on planning

---
+++ birdyboard.py
@@ -0,0 +1,82 @@
+# PLANNING:
+# This is how all of my screen's should look when I finish the app.
+# I wanted to map them out here so that I could plan how to group my functions
+# Started on my testing.py file
+
+# WELCOME TO BIRDYBOARD!
+# ~~~~~~~~~~~~~~~~~~~~~~
+# 1. New User Account
+# 2. Select User
+# 3. View Public Chirps
+# 4. Exit
+# >
+
+# PUBLIC CHIRPS: READ ONLY!
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# (list of public chirps and comments, on viewing will be given back option)
+#  - - - - - - -
+# 9. Main Menu
+# >
+
+# NEW USER
+# ~~~~~~~~
+# Enter full name: (goes on to the next screen, maintains header)
+# >
+
+# Enter screen name:
+# >
+
+# SELECT USER
+# ~~~~~~~~~~~
+# Please tell us your username? (is case sensitive)
+# >
+# (on error, user gets the option to try again, register a user, or exit)
+
+# WELCOME {USER}!
+# ~~~~~~~~~~~~~~~
+# 1. View All Chirps
+# 2. New Public Chirp
+# 3. New Private Chirp
+# 4. Exit
+# >
+
+# CHIRPS: PUBLIC AND PRIVATE
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# PUBLIC CHIRPS:
+# 1. (list of chirps)
+#  - - - - - - -
+# PRIVATE CHIRPS:
+# 1. (list of chirps)
+#  - - - - - - -
+# 9. Main Menu
+# >
+
+# CHIRP THREAD STARTED BY {USER} (public and private)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# - (original chirp and comments)
+#  - - - - - - -
+# 1. Reply
+# 2. Back
+# >
+
+# NEW CHIRP: PUBLIC
+# ~~~~~~~~~~~~~~~~~
+# Enter chirp text:
+# >
+
+# NEW CHIRP: PRIVATE
+# ~~~~~~~~~~~~~~~~~~
+# Chirp at:
+# (list of users)
+#  - - - - - - -
+# 9. Cancel
+# >
+
+# Enter chirp text:
+# >
+
+# SUCCESS!
+# ~~~~~~~~
+# Chirp has been posted!
+
+# 1. Main Menu
---
+++ birdyboard_test.py
@@ -0,0 +1,10 @@
+class TestBehavior(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(self):
+  """setting up test class"""
+
+
+
+if __name__ == '__main__':
+  unittest.main()
